f951795b4ee4efd5a8f9e23424cc344191228fd3
karl wernsman <wernsmank@gmail.com>
2022-10-11 12:31:05 -0700

    Supabase connected via api and url

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,5 +1,6 @@
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://rzhxqrqpkocpayxwaufn.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ6aHhxcnFwa29jcGF5eHdhdWZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjU1MTQwOTUsImV4cCI6MTk4MTA5MDA5NX0.gUQd8rmoDoBHttWYnX7Iw6hWfyrKezsfb07WF0eMyyw';
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 /* Auth related functions */
